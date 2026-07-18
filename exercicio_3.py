171733c4768443a761b868f309035255b6eb04e9
camilaAlsantos <camilaalmeidasantoscons@gmail.com>
2026-07-18 07:57:37 -0300

    Add logic to filter odd numbers from the list

--- exercicio_3.py
+++ exercicio_3.py
@@ -23,3 +23,11 @@ Os números ímpares são [1, 3, 5]
 lista = [*map(int, input("Digite a sua lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui...
+
+impares = []
+
+for numero in lista:
+    if numero % 2 != 0:
+        impares.append(numero)
+
+print(f"Os números ímpares são {impares}")
